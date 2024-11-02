051ef7a46a2afbddd92c41166d21a3687d60d7c1
kei0299 <kitagen_1026@yahoo.co.jp>
2024-11-03 01:07:59 +0900

    corsのURL変更

--- back/config/initializers/cors.rb
+++ back/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8000', '127.0.0.1:8000', 'https://BalanceDays-git-main-keis-projects-a96636b3.vercel.app'
+    origins 'localhost:8000', '127.0.0.1:8000', 'https://myapp-three-rho.vercel.app/'
 
     resource "*",
       headers: :any,
