f43403f5ebdb0906e1245f646f96a05166ca3925
kei0299 <kitagen_1026@yahoo.co.jp>
2024-11-03 22:42:31 +0900

    cors修正

--- back/config/initializers/cors.rb
+++ back/config/initializers/cors.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8000', '127.0.0.1:8000', 'https://myapp-three-rho.vercel.app'
+    # origins 'localhost:8000', '127.0.0.1:8000', 'https://myapp-three-rho.vercel.app'
+    origins "*"
 
     resource "*",
       headers: :any,
