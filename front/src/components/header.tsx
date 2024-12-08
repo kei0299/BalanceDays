54f8568ece58242950a884bb47f0758e777b4313
kei0299 <kitagen_1026@yahoo.co.jp>
2024-12-08 12:39:02 +0900

    不要変数の削除

--- front/src/components/header.tsx
+++ front/src/components/header.tsx
@@ -3,7 +3,6 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
 
 export default function MenuAppBar() {
   return (
