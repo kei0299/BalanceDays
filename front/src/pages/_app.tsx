018d5dcb70c3e46e47a21cc2973d60ed228dc267
kei0299 <kitagen_1026@yahoo.co.jp>
2024-12-08 15:30:48 +0900

    googleアナリティクス導入

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -5,9 +5,22 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { checkSession } from "@/utils/auth/checkSession";
 import { AppContext } from "next/app";
+import Script from "next/script";
+import * as gtag from "@/lib/gtag";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const router = useRouter();
+
+  useEffect(() => {
+    // Google Analytics のページビュー計測
+    const handleRouterChange = (url: any) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouterChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouterChange);
+    };
+  }, [router.events]);
 
   useEffect(() => {
     const AuthCheck = async () => {
@@ -21,18 +34,31 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
     };
 
     AuthCheck(); // 初期ロード時のチェック
-
-    router.beforePopState(({ url }) => {
-      const publicPages = ["/", "/_error", "/signin", "/signup"];
-      if (!publicPages.includes(url)) {
-        AuthCheck();
-        return false;
-      }
-      return true;
-    });
   }, [router]);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Google Analytics Script */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+          `,
+        }}
+      />
+      {/* Main Component */}
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 MyApp.getInitialProps = async (appContext: AppContext) => {
@@ -45,7 +71,7 @@ MyApp.getInitialProps = async (appContext: AppContext) => {
         appContext.ctx.res.statusCode = 302;
         appContext.ctx.res.setHeader("Location", "/signin");
       }
-      return {};
+      return { pageProps: {} };
     }
   }
 
