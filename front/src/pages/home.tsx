b98dc0ba1eb7ca3ed17a6ec6a8d5cd72e9ee37c9
kei0299 <kitagen_1026@yahoo.co.jp>
2024-12-31 14:50:02 +0900

    homeにグラフを移行

--- front/src/pages/home.tsx
+++ front/src/pages/home.tsx
@@ -5,6 +5,8 @@ import { useState, useEffect } from "react";
 // import { checkSession } from "@/utils/auth/checkSession";　// セッション情報確認用
 import Image from "next/image";
 import { fetchCharacter } from "@/utils/auth/fetchCharacter";
+import * as React from "react";
+import { LineChart } from "@mui/x-charts/LineChart";
 
 export default function Home() {
   const currentMonth: Date = new Date();
@@ -48,7 +50,7 @@ export default function Home() {
       <Header />
       <title>BalanceDays</title>
       <link rel="icon" href="/favicon.ico" />
-      <div>
+      <div style={{ height: "100vh", overflow: "hidden", position: "relative" }}>
         <main>
           <Box
             sx={{
@@ -56,57 +58,82 @@ export default function Home() {
               flexDirection: "column",
               justifyContent: "center",
               alignItems: "center",
-              minHeight: "100vh",
               textAlign: "center",
+              height: "100vh",
             }}
           >
-            {/* <div className={`fixed top-0 left-0 w-full h-screen z-[100]`}>
-              <Image
-                src="/image/home.jpg"
-                alt="Sample Image"
-                width={1670}
-                height={800}
-                style={{ marginTop: "-5px" }}
-              />
-            </div> */}
-            <div className={`fixed top-0 left-0 w-full h-screen z-[100]`}>
-              {/* characterStatusの値に応じて画像を切り替え */}
+            <div
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                textAlign: "center",
+              }}
+            >
               {characterStatus === 3 ? (
-                 <>
-                <h1 style={{ color: '#ff4500' }}>あと{life}ヶ月生活できそうです</h1>
-                <Image
-                  src="/image/warning.png"
-                  alt="Character 1"
-                  width={300}
-                  height={300}
-                />
+                <>
+                  <h1 style={{ color: "#ff4500", marginBottom: "20px" }}>
+                    あと{life}ヶ月生活できそうです
+                  </h1>
+                  <Image
+                    src="/image/warning.png"
+                    alt="Character 1"
+                    width={300}
+                    height={300}
+                  />
                 </>
               ) : characterStatus === 2 ? (
                 <>
-                <h1 style={{ color: '#ffd700' }}>あと{life}ヶ月生活できそうです</h1>
-                <Image
-                  src="/image/caution.png"
-                  alt="Character 2"
-                  width={300}
-                  height={300}
-                />
+                  <h1 style={{ color: "#ffd700", marginBottom: "20px" }}>
+                    あと{life}ヶ月生活できそうです
+                  </h1>
+                  <Image
+                    src="/image/caution.png"
+                    alt="Character 2"
+                    width={300}
+                    height={300}
+                  />
                 </>
               ) : characterStatus === 1 ? (
                 <>
-                <h1 style={{ color: '#4169e1' }}>あと{life}ヶ月生活できそうです</h1>
-                <Image
-                src="/image/stable.png"
-                alt="Character 3"
-                width={300}
-                height={300}
-              />
-              </>
+                  <h1 style={{ color: "#4169e1", marginBottom: "20px" }}>
+                    あと{life}ヶ月生活できそうです
+                  </h1>
+                  <Image
+                    src="/image/stable.png"
+                    alt="Character 3"
+                    width={300}
+                    height={300}
+                  />
+                </>
               ) : (
-                <p>設定→生存期間設定から情報を登録してください。<br />
+                <p>
+                  設定→生存期間設定から情報を登録してください。
+                  <br />
                   予算を設定することで今月の予算から過去12ヶ月分を取得し、生存期間を算出します。
                 </p>
               )}
             </div>
+
+            <Box
+              sx={{
+                position: "absolute",
+                bottom: "50px",
+                left: "20px",
+              }}
+            >
+              <LineChart
+                xAxis={[{ data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] }]}
+                series={[
+                  {
+                    data: [10000, 9000, 8000,7000,6000,5000],
+                  },
+                ]}
+                width={400}
+                height={300}
+              />
+            </Box>
           </Box>
         </main>
         <FooterLogin />
