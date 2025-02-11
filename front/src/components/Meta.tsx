dcd4e26362ef0c57c4864fd9ced9fa1fc1de0206
kei0299 <kitagen_1026@yahoo.co.jp>
2025-02-11 16:59:12 +0900

    OGPのアプリ説明変更

--- front/src/components/Meta.tsx
+++ front/src/components/Meta.tsx
@@ -3,8 +3,8 @@ import Head from "next/head";
 const Meta = () => {
   return (
     <Head>
-      <title>BalanceDays</title>
-      <meta property="og:title" content="BalanceDays | 家計簿で生活可能日数を算出" />
+      <title>BalanceDays | 家計簿で生活可能日数を算出</title>
+      <meta property="og:title" content="BalanceDays" />
       <meta property="og:description" content="貯金残高から、あと何日生活できるかをシンプルに計算。未来の計画を立てやすくする便利なアプリです。" />
       <meta property="og:image" content="https://myapp-three-rho.vercel.app/image/stable.png" />
       <meta property="og:type" content="website" />
