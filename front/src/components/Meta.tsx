dfd07fc64534d464c5ce0d3d068a465c7e141f73
kei0299 <kitagen_1026@yahoo.co.jp>
2025-02-11 16:35:34 +0900

    OGP修正

--- front/src/components/Meta.tsx
+++ front/src/components/Meta.tsx
@@ -7,7 +7,7 @@ const Meta = () => {
       <meta name="description" content="このサイトの説明" />
       <meta property="og:title" content="BalanceDays" />
       <meta property="og:description" content="貯金残高からあとどのくらい生活できるかを判定してくれるアプリです。" />
-      <meta property="og:image" content="/image/stable.png" />
+      <meta property="og:image" content="https://myapp-three-rho.vercel.app/image/stable.png" />
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://myapp-three-rho.vercel.app/" />
     </Head>
