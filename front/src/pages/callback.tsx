84702a8bad94199b4e6d05f1de6c9911b9fa34a1
kei0299 <kitagen_1026@yahoo.co.jp>
2025-02-13 00:22:52 +0900

    googleログインリダイレクト確認

--- front/src/pages/callback.tsx
+++ front/src/pages/callback.tsx
@@ -1,14 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { checkSession } from "@/utils/auth/checkSession";
+
+export default function Index() {
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchSessionData = async () => {
       try {
-        const sessionData = await checkSession(); // checkSessionの結果を取得
+        const sessionData = await checkSession();
         console.log(sessionData);
       } catch (error) {
         console.error("セッションチェックエラー", error);
+      } finally {
+        setLoading(false);
       }
     };
     fetchSessionData();
   }, []);
+
+  return <div>{loading ? "ログイン処理中..." : "完了"}</div>;
+}
