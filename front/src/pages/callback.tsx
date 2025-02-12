e107327b788dd4004437b3e05e69ff986080759e
kei0299 <kitagen_1026@yahoo.co.jp>
2025-02-13 00:11:03 +0900

    googleログインリダイレクト確認

--- front/src/pages/callback.tsx
+++ front/src/pages/callback.tsx
@@ -1,72 +1,14 @@
-import { setCookie } from "nookies";
 import { useEffect } from "react";
-import { useRouter } from "next/router";
-
-const setAccessToken = (accessToken: string, client: string, uid: string) => {
-  setCookie(null, "accessToken", accessToken, {
-    maxAge: 30 * 24 * 60 * 60,
-    path: "/",
-  });
-  setCookie(null, "client", client, {
-    maxAge: 30 * 24 * 60 * 60,
-    path: "/",
-  });
-  setCookie(null, "uid", uid, {
-    maxAge: 30 * 24 * 60 * 60,
-    path: "/",
-  });
-};
-
-export default function OAuthRedirect() {
-  const router = useRouter();
+import { checkSession } from "@/utils/auth/checkSession";
 
   useEffect(() => {
-    const handleOAuthCallback = async () => {
-      const { auth_token, client_id, uid } = router.query; // URLからパラメータを取得
-
-      if (!auth_token || !client_id || !uid) {
-        console.error("認証情報が不足しています");
-        return;
-      }
-
+    const fetchSessionData = async () => {
       try {
-        const response = await fetch(
-          `${process.env.NEXT_PUBLIC_API_URL}/v1/auth/google_oauth2/callback`,
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              auth_token,
-              client_id,
-              uid,
-            }),
-          }
-        );
-
-        if (!response.ok) {
-          throw new Error("Google OAuth ログインに失敗しました");
-        }
-
-        const accessToken = response.headers.get("access-token");
-        const client = response.headers.get("client");
-        const uidFromResponse = response.headers.get("uid");
-
-        if (accessToken && client && uidFromResponse) {
-          setAccessToken(accessToken, client, uidFromResponse);
-          console.log(accessToken, client, uidFromResponse);
-          // router.push("/home");
-        } else {
-          console.error("必要な認証情報が取得できませんでした");
-        }
+        const sessionData = await checkSession(); // checkSessionの結果を取得
+        console.log(sessionData);
       } catch (error) {
-        console.error("OAuth 処理中にエラー:", error);
+        console.error("セッションチェックエラー", error);
       }
     };
-
-    handleOAuthCallback();
-  }, [router]);
-
-  return <div>ログイン処理中...</div>;
-}
+    fetchSessionData();
+  }, []);
