018a9deba270fa2dfa51d06a779d2c6a98e3dfc6
kei0299 <kitagen_1026@yahoo.co.jp>
2025-02-13 01:18:21 +0900

    googleログインリダイレクト確認

--- front/src/pages/callback.tsx
+++ front/src/pages/callback.tsx
@@ -16,8 +16,9 @@ export default function OAuthRedirect() {
       setCookie(null, "client", client_id, { maxAge: 30 * 24 * 60 * 60, path: "/" });
       setCookie(null, "uid", uid, { maxAge: 30 * 24 * 60 * 60, path: "/" });
 
+      console.log(auth_token, client_id, uid);
       // ホームページへリダイレクト
-      router.push("/home");
+      // router.push("/home");
     } else {
       console.error("必要な認証情報が取得できませんでした");
     }
