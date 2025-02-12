11158e32fcd7f6fc198c7686d243a1b76b5e9f3f
kei0299 <kitagen_1026@yahoo.co.jp>
2025-02-12 20:51:14 +0900

    本番環境にroot追加

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root "home#index"
-  
+  root "/"
+
   namespace :v1 do
     mount_devise_token_auth_for "User", at: "auth", controllers: {
     registrations: 'v1/auth/registrations',
