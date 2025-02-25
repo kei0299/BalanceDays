a37e4938f5038eace51f34c3ccb267a4a65727d4
kei0299 <kitagen_1026@yahoo.co.jp>
2025-02-25 17:57:46 +0900

    ヘッダーにログインを追加

--- front/src/components/header.tsx
+++ front/src/components/header.tsx
@@ -4,6 +4,7 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Link from "next/link";
+import Button from "@mui/material/Button";
 
 export default function MenuAppBar() {
   return (
@@ -11,14 +12,44 @@ export default function MenuAppBar() {
       <AppBar
         position="fixed"
         elevation={0}
-        style={{ color: "#ffffff", backgroundColor: "#9bc0ff" }}
+        sx={{ color: "#ffffff", backgroundColor: "#9bc0ff", padding: "0 16px" }}
       >
         <Toolbar>
-          <Link href="/" passHref>
-            <Typography variant="h6" sx={{ flexGrow: 1 }}>
-              BalanceDays
-            </Typography>
-          </Link>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            <Link href="/" passHref>
+              <Box
+                component="a"
+                sx={{ textDecoration: "none", color: "inherit" }}
+              >
+                BalanceDays
+              </Box>
+            </Link>
+          </Typography>
+
+          <Box>
+            <Link href="/signup" passHref>
+              <Button
+                variant="contained"
+                sx={{
+                  marginRight: 1,
+                  backgroundColor: "#4169e1",
+                }}
+              >
+                新規作成
+              </Button>
+            </Link>
+            <Link href="/signin" passHref>
+              <Button
+                variant="contained"
+                sx={{
+                  marginRight: 1,
+                  backgroundColor: "#4169e1",
+                }}
+              >
+                ログイン
+              </Button>
+            </Link>
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
