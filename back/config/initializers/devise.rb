a1b72066c5a10e9cd91374feae43a4530b313014
kei0299 <kitagen_1026@yahoo.co.jp>
2025-03-03 17:41:42 +0900

    google auth 修正

--- back/config/initializers/devise.rb
+++ back/config/initializers/devise.rb
@@ -313,6 +313,7 @@ Devise.setup do |config|
 
   config.omniauth :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {
     scope: 'email,profile',
+    prompt: 'select_account',
     redirect_uri: 'https://myapp-7eck.onrender.com/omniauth/google_oauth2/callback'
   }
 end
