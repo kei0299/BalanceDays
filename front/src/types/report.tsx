2325ba1c81cb1590d7cb7693143ba38fc17b14a8
kei0299 <kitagen_1026@yahoo.co.jp>
2025-03-21 00:47:47 +0900

    reportにimportを追加

--- front/src/types/report.tsx
+++ front/src/types/report.tsx
@@ -1,12 +1,12 @@
 // 円グラフ用の型定義
-interface pieChartData {
+export interface pieChartData {
   id: number;
   current_month_amount: number;
   name: string;
 }
 
 // expense_categoryの型定義
-interface expenseCategoryData {
+export interface expenseCategoryData {
   id: number;
   name: string;
 }
